5bb33cc119d8c7ecd4ad3a1c2e4e360d5e20001f
Kári <kari@Karis-MacBook-Air-2.local>
2023-03-02 20:20:16 +0000

    Starting on V%

---
+++ HD6/src/V5/MeasureBST.java
@@ -0,0 +1,24 @@
+import edu.princeton.cs.algs4.*;
+
+public class MeasureBST {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        for (int t = 0; t < trials; t++) {
+            
+            // Smíða tvíleitartré ...
+
+
+            // Setja n slembnar double tölur inn í það ...
+
+
+            // Finna hæð trésins ...
+
+        }
+
+        // Prenta út niðurstöður...
+
+    }
+}
