bf8d0f6f4c65e483903137c27d2bcfdba60a2b4d
Kári <kari@Karis-MacBook-Air-2.local>
2023-03-10 00:56:17 +0000

    Final commit

--- HD7/src/V5/analysis.R
+++ HD7/src/V5/analysis.R
@@ -1,6 +1,9 @@
-library(tidyverse)
-
-header <- c("n", "trials", "type", "opt_height", "avg_height", "avg_time", "sd_time")
-
-df <- read.csv("~/Github/TOL203G/HD7/src/V5/data.csv", sep="\t", header=FALSE)
-colnames(df) <- header
+  library(tidyverse)
+  
+  header <- c("n", "trials", "type", "opt_height", "avg_height", "avg_time", "sd_time")
+  
+  df <- read.csv("~/Github/TOL203G/HD7/src/V5/data.csv", sep="\t", header=FALSE)
+  
+  
+  colnames(df) <- header
+  df$type <- trimws(df$type)
