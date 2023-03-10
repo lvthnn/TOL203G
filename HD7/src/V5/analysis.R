4823569469a36180cea11ce977cd6ddb4c49540c
Kári Hlynsson <63433562+lvthnn@users.noreply.github.com>
2023-03-10 13:27:52 +0000

    Delete analysis.R

--- HD7/src/V5/analysis.R
+++
@@ -1,9 +0,0 @@
-  library(tidyverse)
-  
-  header <- c("n", "trials", "type", "opt_height", "avg_height", "avg_time", "sd_time")
-  
-  df <- read.csv("~/Github/TOL203G/HD7/src/V5/data.csv", sep="\t", header=FALSE)
-  
-  
-  colnames(df) <- header
-  df$type <- trimws(df$type)
